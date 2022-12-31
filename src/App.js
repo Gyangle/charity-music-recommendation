e93360359ee4f8fc972b5db79d761f8e6fd3898d
GrantY <gyangle@umich.edu>
2022-12-30 20:50:29 -0500

    create template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <LyricsDiaplay />
+      <AmplifyBackendTesting />
     </div>
   );
 }
 
-export default App;
+const Header = () => {
+  return <p>Search Bar</p>;
+};
+
+const LyricsDiaplay = () => {
+  return <p>Display Lyrics</p>;
+};
+
+const AmplifyBackendTesting = () => {
+  return <p>Backend</p>
+}
